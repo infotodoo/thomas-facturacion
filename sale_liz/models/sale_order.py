1903bf8f0e0b0de1421c8bcc140b6e798bcc50d6
cgtodoo <64793177+cgtodoo@users.noreply.github.com>
2020-10-28 18:39:44 -0500

    Update sale_order.py

--- sale_liz/models/sale_order.py
+++ sale_liz/models/sale_order.py
@@ -7,8 +7,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     # _description = 'practica/sale_liz.practica/sale_liz'
 
-    state = fields.Selection(selection_add=[('1','BORRADOR'),('2','COTIZACION'),('3','APROBACION 1'),('4','APROBACION 2'),('5','APROBACION 3'),('6','ORDEN DE VENTA')
-                            ,('7','A FACTURAR'),('8','NOTA CREDITO'),('9','NOTA DEBITO'),('10','DOCUMENTO RFC'),('11','FACTURADO')],string='state')
+    state = fields.Selection(selection_add=[('1','BORRADOR'),('2','COTIZACION'),('5','APROBACION'),('6','ORDEN DE VENTA')
+                            ,('7','A FACTURAR'),('8','NOTA CREDITO'),('9','NOTA DEBITO'),('10','DOCUMENTO RFC'),('11','UNIONES TEMPORALES'),('12','FACTURADO')],string='state')
     # value = fields.Integer()
     # value2 = fields.Float(compute="_value_pc", store=True)
     # description = fields.Text()
